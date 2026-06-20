f7ab41b61d4299748798cc41b24fd3759b931eba
stiverthijmen163 <t.m.a.broeren@student.tue.nl>
2026-06-20 23:21:42 +0200

    small fix

--- world/state.py
+++ world/state.py
@@ -57,7 +57,7 @@ class ObservationBuilder:
         if self.obs_mode == "xy":
             return np.array([x, y], dtype=np.float32)
 
-        sensors = self.get_sensor_readings(x, y) / self.sensor_range
+        sensors = self.get_sensor_readings(x, y)
 
         if self.obs_mode == "sensors":
             return sensors
